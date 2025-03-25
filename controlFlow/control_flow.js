97f9b80642475ca1f7a55384ba73abb246eec60f
jaiyesh1918 <jaiyesh1918@gmail.com>
2025-03-25 03:41:37 -0400

    teesko

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,23 @@
+let userRole="admin"
+let accessLevel
+let isLoggedIn = true;
+let userMessage;
+if (userRole==="admin"){
+    accessLevel="Full access granted"
+}
+else if (userRole==="manager"){
+    accessLevel="Limited access granted"
+}
+else{
+    accessLevel="No access granted"
+}
+console.log("access level :"+accessLevel)
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}console.log("User Message:", userMessage);
